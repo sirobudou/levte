aed6b3ad902ae5aa1c42bbbddd6a7329230799fc
sirobudou <take263230bsk@gmail.com>
2023-10-18 11:17:40 +0900

    ブログ投稿の作成

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+        //getPaginateByLimit()はPost.phpで定義したメソッドです。
+    }
+    /**
+    * 特定IDのpostを表示する
+    *
+    * @params Object Post // 引数の$postはid=1のPostインスタンス
+    * @return Reposnse post view
+    */
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+}
+?>
